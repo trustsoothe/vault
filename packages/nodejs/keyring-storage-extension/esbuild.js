17fe20617623757e96378075be77eac754d86e55
Julio Henriquez <julio.m.henriquez@gmail.com>
2023-10-04 19:17:34 -0400

    Adds all necessary polyfills in order to use web3-eth. Updates the common build script easier support of optionsOverride. Updates kerying-storage-extension build make all imports external.

--- packages/nodejs/keyring-storage-extension/esbuild.js
+++ packages/nodejs/keyring-storage-extension/esbuild.js
@@ -1,2 +1,13 @@
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
 const esbuildBase = require('@poktscan/tsconfig/esbuild.base')
-esbuildBase.run()
+
+const configOverrides = {
+    plugins: [
+        /*
+         * Consider all imports external packages. So that this does not import the whole keyring because of single class.
+         */
+        nodeExternalsPlugin(),
+    ],
+}
+
+esbuildBase.run(configOverrides)
