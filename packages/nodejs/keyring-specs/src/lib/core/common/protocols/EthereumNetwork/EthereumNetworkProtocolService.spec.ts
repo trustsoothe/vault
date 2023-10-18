3c8319830a00bf8075cbc0a43be38bb5d1c9f5d7
Alan Rojas <alan2rm7@gmail.com>
2023-10-18 10:53:15 -0400

    fixed duplicated import

--- packages/nodejs/keyring-specs/src/lib/core/common/protocols/EthereumNetwork/EthereumNetworkProtocolService.spec.ts
+++ packages/nodejs/keyring-specs/src/lib/core/common/protocols/EthereumNetwork/EthereumNetworkProtocolService.spec.ts
@@ -1,43 +1,43 @@
-import {describe} from "vitest";
-import ProtocolServiceSpecFactory from '../IProtocolService.specFactory';
+import { describe } from "vitest";
+import ProtocolServiceSpecFactory from "../IProtocolService.specFactory";
 import {
   AccountReference,
   Asset,
   EthereumNetworkProtocolService,
-  IEncryptionService,
   Network,
   SupportedProtocols,
   IEncryptionService,
 } from "@poktscan/keyring";
-import {WebEncryptionService} from "@poktscan/keyring-encryption-web";
+import { WebEncryptionService } from "@poktscan/keyring-encryption-web";
 
-describe('EthereumNetworkProtocolService', () => {
+describe("EthereumNetworkProtocolService", () => {
   const asset: Asset = new Asset({
-    name: 'Ethereum Sepolia',
+    name: "Ethereum Sepolia",
     protocol: SupportedProtocols.Ethereum,
-    symbol: 'ETH'
-  })
+    symbol: "ETH",
+  });
 
   const network = new Network<SupportedProtocols.Ethereum>({
-    name: 'test',
-    rpcUrl: 'http://localhost:8080',
+    name: "test",
+    rpcUrl: "http://localhost:8080",
     protocol: asset.protocol,
-    chainID: '11155111',
-  })
+    chainID: "11155111",
+  });
 
-  const account =
-    new AccountReference(
-      'account-id',
-      'test-account',
-      '0x3F56d4881EB6Ae4b6a6580E7BaF842860A0D2465',
-      asset,
-    );
+  const account = new AccountReference(
+    "account-id",
+    "test-account",
+    "0x3F56d4881EB6Ae4b6a6580E7BaF842860A0D2465",
+    asset
+  );
 
   const accountImport = {
-    privateKey: 'e65700becfed73028e0d00d81217e9bfd5db4af9cbc960493b6ffa5633e98797',
-    publicKey: '0x7ff21bc4f68979598e3f9e47bb814a9a3115678b0a577050af08bcb2af0826cb16d4901b7e913f05dcdc57b874bc9f73e8ebe08737704e2c005398466a8f918f',
-    address: '0x3F56d4881EB6Ae4b6a6580E7BaF842860A0D2465',
-  }
+    privateKey:
+      "e65700becfed73028e0d00d81217e9bfd5db4af9cbc960493b6ffa5633e98797",
+    publicKey:
+      "0x7ff21bc4f68979598e3f9e47bb814a9a3115678b0a577050af08bcb2af0826cb16d4901b7e913f05dcdc57b874bc9f73e8ebe08737704e2c005398466a8f918f",
+    address: "0x3F56d4881EB6Ae4b6a6580E7BaF842860A0D2465",
+  };
 
   const encryptionService: IEncryptionService = new WebEncryptionService();
   const protocolService = new EthereumNetworkProtocolService(encryptionService);
@@ -45,5 +45,5 @@ describe('EthereumNetworkProtocolService', () => {
   ProtocolServiceSpecFactory<SupportedProtocols.Ethereum>(
     () => protocolService,
     { asset, network, account, accountImport }
-  )
-})
+  );
+});
