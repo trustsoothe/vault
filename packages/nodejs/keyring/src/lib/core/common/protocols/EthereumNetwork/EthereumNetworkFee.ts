30724d13a8fc3abe21e2db4bd922a77a79cfec24
Julio Henriquez <julio.m.henriquez@gmail.com>
2023-10-23 16:54:59 -0400

    feature/ethereum-support - WIP - Update the Ethereum fee response object

--- packages/nodejs/keyring/src/lib/core/common/protocols/EthereumNetwork/EthereumNetworkFee.ts
+++ packages/nodejs/keyring/src/lib/core/common/protocols/EthereumNetwork/EthereumNetworkFee.ts
@@ -2,9 +2,19 @@ import {IAbstractProtocolFee} from "../ProtocolFee";
 import {SupportedProtocols} from "../../values";
 
 export interface EthereumNetworkFee extends IAbstractProtocolFee<SupportedProtocols.Ethereum> {
-  gasPrice: bigint;
-  gasLimit: bigint;
-  suggestedLow: bigint;
-  suggestedMedium: bigint;
-  suggestedHigh: bigint;
+  low: {
+    suggestedMaxFeePerGas: bigint;
+    suggestedMaxPriorityFeePerGas: bigint;
+    amount: bigint;
+  };
+  medium: {
+    suggestedMaxFeePerGas: bigint;
+    suggestedMaxPriorityFeePerGas: bigint;
+    amount: bigint;
+  };
+  high: {
+    suggestedMaxFeePerGas: bigint;
+    suggestedMaxPriorityFeePerGas: bigint;
+    amount: bigint;
+  };
 }
