84e8e5aa2ae59efbc2d4dd04cafad741c8659d05
Julio Henriquez <julio.m.henriquez@gmail.com>
2024-03-06 19:38:46 -0400

    features/adds-evm-protocols - Removes the chainID validations. Adds the content-type to ethereum raw transaction request. Some of the chains where very picky about the that.

--- packages/nodejs/vault/src/lib/core/common/protocols/EthereumNetwork/schemas.ts
+++ packages/nodejs/vault/src/lib/core/common/protocols/EthereumNetwork/schemas.ts
@@ -3,7 +3,6 @@ import {ETHEREUM_PROTOCOL} from "../../values";
 
 export const EthereumProtocolNetworkSchema = z.object({
   protocol: z.literal(ETHEREUM_PROTOCOL),
-  chainID: z.enum(['11155111', '5', '1']),
   rpcUrl: z.string().url(),
 });
 
